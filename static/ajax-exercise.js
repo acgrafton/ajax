3c1f2334f26b8d267c544e8cc4c48a498b57e100
Angela Grafton <ayeung06@gmail.com>
2020-05-22 08:15:11 -0600

    Completed Parts 2-3

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -16,15 +16,16 @@ $('#weather-form').on('submit', (evt) => {
 
   const formData = {
     // TODO: select the zipcode input
-    zipcode: $('REPLACE THIS').val()
+    'zipcode': $('#zipcode-field').val()
   };
 
   // TODO: choose a request method (GET or POST) by uncommenting one of
   // these blocks of code
 
-  // $.get('/replaceMe', formData, (response) => {
-  //   // Fill in the callback function
-  // });
+  $.get('/weather', formData, (response) => {
+    $('#weather-form').html(`${response.forecast}`);
+
+  });
 
   // $.post('/replaceMe', formData, (response) => {
   //   // Fill in the callback function
@@ -39,9 +40,15 @@ $("#order-form").on('submit', (evt) => {
 
   // TODO: create an object to store key-value pairs that'll be sent to
   // the server
-
+  const formData = {
+    'qty': $('#qty-field').val(),
+    'melon_type': $('#melon-type-field').val()
+  }
   // TODO: make a request to /order-melons
-  //
+  $.post('/order-melons', formData, (response) => {
+    console.log(response.code);
+    $('#order-status').html(`${response.code} ${response.msg}`);
+  });
   // In the callback function, use the response from the server to
   // update #order-status. IMPORTANT: if the result code is 'ERROR',
   // make it show up in red.
